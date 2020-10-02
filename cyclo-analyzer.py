a35dd7340ec5f300d67266770bf1d32a540ddcfc
pholtz <pholtz@ycp.edu>
2020-10-02 10:28:47 -0400

    add initial hist plots, update seaborn, and apply default themes to all existing plots

--- cyclo-analyzer.py
+++ cyclo-analyzer.py
@@ -68,6 +68,14 @@ def main():
         help="Plot average distances for each day of the week (bar)")
     adow_command.set_defaults(func=average_distance_over_weekday)
 
+    distance_histogram_command = subparsers.add_parser("dhist",
+        help="Plot the distribution of ride distances for all time")
+    distance_histogram_command.set_defaults(func=distance_histogram)
+
+    moving_time_histogram_command = subparsers.add_parser("thist",
+        help="Plot the distribution of ride times for all time")
+    moving_time_histogram_command.set_defaults(func=moving_time_histogram)
+
     ### Miscellaneous ###
     dump_command = subparsers.add_parser("dump",
         help="Applies a specified transform to the activities file, for readability or compatibility with another system")
@@ -229,13 +237,13 @@ def speed_over_time(arguments):
         "datetime": [point[0] for point in times_and_speeds],
         "speed": [point[1] for point in times_and_speeds]
     })
-    speed_dataframe["bin_speed"] = speed_dataframe.rolling(window=10).mean()
+    speed_dataframe["bin_speed"] = speed_dataframe.rolling(window=15).mean()
 
-    avg_palette = seaborn.color_palette("rocket")
-    avg_plot = seaborn.lineplot(x="datetime", y="bin_speed", data=speed_dataframe, palette=avg_palette)
-    avg_plot.set(xlabel="Time", ylabel="Speed (meters / second)")
+    seaborn.set_theme()
+    avg_plot = seaborn.lineplot(x="datetime", y="bin_speed", data=speed_dataframe)
+    avg_plot.set(xlabel="Time", ylabel="Speed (miles / hour)")
     
-    # plt.fill_between(speed_dataframe.datetime.values, speed_dataframe.speed.values)
+    plt.fill_between(speed_dataframe.datetime.values, speed_dataframe.bin_speed.values)
     plt.savefig("speed.png")
     plt.show()
 
@@ -269,6 +277,8 @@ def elevation_over_time(arguments):
         "datetime": [point[0] for point in trackpoints],
         "elevation": [point[1] for point in trackpoints]
     })
+
+    seaborn.set_theme()
     elevation_plot = seaborn.lineplot(
         x="datetime", y="elevation", data=elevation_dataframe)
     elevation_plot.set(xlabel="Time", ylabel="Elevation (meters)")
@@ -284,8 +294,7 @@ def average_distance_over_weekday(arguments):
     weekdays_by_index = dict(zip(range(7), calendar.day_name))
     distances_by_index = dict(zip(range(7), [[] for x in range(7)]))
     for activity in rides:
-        activity_datetime = datetime.datetime.strptime(activity.date, "%b %d, %Y, %I:%M:%S %p")
-        distances_by_index[activity_datetime.weekday()].append(float(activity.distance) * 0.000621371)
+        distances_by_index[activity.date.weekday()].append(float(activity.distance) * 0.000621371)
 
     average_distances = [statistics.mean(weekday_distances) for index, weekday_distances in distances_by_index.items()]
 
@@ -293,7 +302,8 @@ def average_distance_over_weekday(arguments):
         "weekday": [weekdays_by_index[index] for index, distance in enumerate(average_distances)],
         "distances": average_distances
     })
-    print(adow_df)
+    
+    seaborn.set_theme()
     adow_plot = seaborn.barplot(x="weekday", y="distances", data=adow_df)
     adow_plot.set(xlabel="Day of Week", ylabel="Average Distance (miles)")
     plt.savefig("adow.png")
@@ -311,6 +321,8 @@ def elevation_time_speed(arguments):
         "moving_time": [float(activity.moving_time) / 60 for activity in rides],
         "average_speed": [float(activity.average_speed) * 2.237 if activity.average_speed else 0 for activity in rides]
     })
+
+    seaborn.set_theme()
     ets_pivot = pd.pivot_table(ets_df, index="elevation", columns="moving_time", values="average_speed", aggfunc=np.average)
     f, ax = plt.subplots(figsize=(9, 6))
     ets_plot = seaborn.heatmap(ets_pivot, annot=True, linewidths=0.5, ax=ax)
@@ -325,8 +337,10 @@ def average_speed_over_activities(arguments):
 
     asot_df = pd.DataFrame(data={
         "activity_date": [activity.date for activity in rides],
-        "average_speed": [float(activity.average_speed) * 2.237 if activity.average_speed else 0 for activity in rides]
+        "average_speed": [activity.average_speed if activity.average_speed else 0 for activity in rides]
     })
+
+    seaborn.set_theme()
     asot_plot = seaborn.lineplot(x="activity_date", y="average_speed", data=asot_df)
     asot_plot.set(xlabel="Date", ylabel="Average Speed (mph)")
     plt.fill_between(asot_df.activity_date.values, asot_df.average_speed.values)
@@ -342,12 +356,47 @@ def distance_over_time(arguments):
 
     dot_by_id = {
         "distance": [ride.distance for ride in rides],
-        "moving_time": [ride.moving_time / 60 for ride in rides]
+        "moving_time": [ride.moving_time / 60 for ride in rides],
+        "average_speed": [ride.average_speed for ride in rides]
     }
+
+    seaborn.set_theme()
     dot_df = pd.DataFrame(data=dot_by_id)
-    dot_plot = seaborn.relplot(x="moving_time", y="distance", data=dot_df)
+    dot_plot = seaborn.lmplot(x="moving_time", y="distance", data=dot_df)
     dot_plot.set(xlabel="Moving Time (Minutes)", ylabel="Distance (Miles)")
     plt.savefig("dot.png")
+    plt.show()
+
+
+def distance_histogram(arguments):
+    activities = parse_activities_csv()
+    rides = [activity for activity in activities if activity.activity_type == "Ride"]
+    rides = [ride.convert_to_imperial() for ride in rides]
+
+    distance_df = pd.DataFrame(data={
+        "distance": [ride.distance for ride in rides]
+    })
+    
+    seaborn.set_theme()
+    distance_plot = seaborn.displot(distance_df, x="distance", binwidth=1)
+    distance_plot.set(xlabel="Distance (miles)", ylabel="Count")
+    plt.savefig("dhist.svg")
+    plt.show()
+
+
+def moving_time_histogram(arguments):
+    activities = parse_activities_csv()
+    rides = [activity for activity in activities if activity.activity_type == "Ride"]
+    rides = [ride.convert_to_imperial() for ride in rides]
+
+    time_df = pd.DataFrame(data={
+        "moving_time": [ride.moving_time / 60 for ride in rides]
+    })
+
+    seaborn.set_theme()
+    time_plot = seaborn.displot(time_df, x="moving_time", binwidth=5)
+    time_plot.set(xlabel="Moving Time (minutes)", ylabel="Count")
+    plt.savefig("thist.svg")
     plt.show()
 
 
